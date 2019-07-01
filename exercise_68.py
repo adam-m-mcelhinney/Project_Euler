6a0dd7b8d9dd27299a4369a078e85cc7c8c0780d
Adam McElhinney <adam.mcelhinney@uptake.com>
2019-07-01 15:25:02 -0500

    Finally solved 68!

--- exercise_68.py
+++ exercise_68.py
@@ -8,22 +8,27 @@ from numpy import nan
 class gonRing():
 
     def __init__(self):
-        self.numNodes = None
         '''Paths are lists of node numbers that are connected to sum to the magic number
-        By convention, the first elemnet of the list will be the external node. Also, paths
+        By convention, the first element of the list will be the external node. Also, paths
         must be entered clockwise.
         '''
         self.paths = []
         self.numPaths = 0
+        #self.numNodes = 0
         '''Path values are integers that are stored in a given node'''
         self.pathVals = []
-        '''Path totals are the sum of all the path vals ni a given path'''
+        '''Path totals are the sum of all the path vals in a given path'''
         self.pathTotals = []
         self.values = {}
         self.isMagic = False
         self.magicTotal = nan
-        self.externalNodes = []
+        # self.externalNodes = []
         self.magicConcat = nan
+
+
+    def __repr__(self):
+        return(f'paths: {self.paths}, pathVals: {self.pathVals}, pathTotals: {self.pathTotals}, magicTotal: {self.magicTotal}')
+
 
     def add_path(self, pathList):
 
@@ -31,20 +36,21 @@ class gonRing():
         self.numPaths = len(self.paths)
         self.pathVals.append([nan for i in range(len(pathList))])
         self.pathTotals = [nan for i in range(self.numPaths)]
-        self.externalNodes.append(pathList[0])
+        #self.externalNodes.append(pathList[0])
 
     def add_value(self, node, val):
         self.values[node] = val
-        for i,path in enumerate(self.paths):
-            for j, t in enumerate(path):
-                if t == node:
+        # Need to update the path totals
+        for i, path in enumerate(self.paths):
+            for j, pathVal in enumerate(path):
+                if pathVal == node:
                     self.pathVals[i][j] = val
             self.pathTotals[i] = sum(self.pathVals[i])
         self._setmagic()
 
     def _setmagic(self):
         pathTotalSet = set(self.pathTotals)
-        # Not sure what happens if everytihn is nan here
+        # Not sure what happens if everything is nan here
         if len(pathTotalSet) == 1:
             self.isMagic = True
             self.magicTotal = self.pathTotals[0]
@@ -53,15 +59,16 @@ class gonRing():
             self.isMagic = False
             self.magicTotal = nan
             self.magicConcat = nan
-
+    # TODO: This is wrong somehow.
     def _concatenateMagicTotal(self):
         startList = self.pathVals[0]
         startVal = 999
         startIndex = 0
         for i, path in enumerate(self.pathVals):
             if path[0] < startVal:
+                startVal = path[0]
                 startIndex = i
-        concatenateOrder = [(startVal+i)%self.numPaths for i in range(self.numPaths)]
+        concatenateOrder = [(startIndex+i)%self.numPaths for i in range(self.numPaths)]
         concatStr = ''
         for i in concatenateOrder:
             for j in self.pathVals[i]:
@@ -76,48 +83,121 @@ class gonRing():
 
 
 
-
-ring = gonRing()
-paths = [[0, 1, 2], [3, 2, 4], [5, 4, 1]]
-for path in paths:
-    ring.add_path(path)
-
-pathVals = {0:4, 1:3, 2:2, 3:6, 4:1, 5:5}
-
-for node in pathVals.keys():
-    ring.add_value(node=node, val = pathVals[node])
-
-print(ring.pathVals)
-print(ring.paths)
-print(ring.numPaths)
-print(ring.pathVals)
-print(ring.pathTotals)
-print(ring.isMagic)
-print(ring.magicTotal)
-print(ring.externalNodes)
-print(ring.magicConcat)
+# print(ring.pathVals)
+# print(ring.paths)
+# print(ring.numPaths)
+# print(ring.pathVals)
+# print(ring.pathTotals)
+# print(ring.isMagic)
+# print(ring.magicTotal)
+# print(ring.magicConcat)
 
 
-pathVals = {0:4, 1:3, 2:2, 3:6, 4:1, 5:999}
-for node in pathVals.keys():
-    ring.add_value(node=node, val = pathVals[node])
-
-
-# TODO: Need to work on this part
-from itertools import permutations
-testVals = permutations(list(range(5)))
-allPaths = []
-for testVal in testVals:
+def createTestCase(testString, paths):
+    '''The website lists things in teh form
+    9	4,2,3; 5,3,1; 6,1,2
+    magicTotal  pathvals (separated by ;)
+    '''
+    splt_str =  testString.split('\t')
+    magicTotal = int(splt_str[0])
+    testVal = splt_str[1].replace(' ', '').replace(';',',').split(',')
+    testVal = [int(i) for i in testVal]
     pathVals = {}
+    flatPath = [node for path in paths for node in path]
     for i, val in enumerate(testVal):
-        pathVals[i] = val
-        ring = gonRing()
-        for node in pathVals.keys():
-            ring.add_value(node=node, val = pathVals[node])
-            if ring.isMagic:
-                print(ring.magicTotal)
-                print(ring.magicConcat)
+        node = flatPath[i]
+        if node not in pathVals:
+            pathVals[node] = val
+    return pathVals, magicTotal, testVal
 
-    allPaths.append(pathVals)
+
+def createRing(pathVals, paths, magicTotal = None, testString = None):
+
+    ring = gonRing()
+    # Add paths
+    for i, path in enumerate(paths):
+        ring.add_path(path)
+    # Add nodes
+    for node in pathVals.keys():
+        ring.add_value(node=node, val = pathVals[node])
+    if magicTotal != None:
+        assert magicTotal == ring.magicTotal
+    if testString != None:
+        assert ring.magicConcat == testString
+    return ring
+
+
+
+
+
+
+
+
+# Taken from the website
+website_solutions =\
+    ['9	4,2,3; 5,3,1; 6,1,2',
+'9	4,3,2; 6,2,1; 5,1,3',
+'10	2,3,5; 4,5,1; 6,1,3',
+'10	2,5,3; 6,3,1; 4,1,5',
+'11	1,4,6; 3,6,2; 5,2,4',
+'11	1,6,4; 5,4,2; 3,2,6',
+'12	1,5,6; 2,6,4; 3,4,5',
+'12	1,6,5; 3,5,4; 2,4,6']
+
+
+for case in website_solutions:
+    pathVals, magicTotal, cleanSolution = createTestCase(case, paths)
+    ring = createRing(pathVals, paths, magicTotal = magicTotal)
+
+
+
+from itertools import permutations
+valuesToTest = permutations(list(range(1,7)))
+nodeList = list(range(6))
+solutions = []
+for i, val in enumerate(valuesToTest):
+    pathVals = dict(zip(nodeList, val))
+    ring = createRing(pathVals, paths)
+    if ring.isMagic:
+        solutions.append(ring)
+
+
+solutionConcats = []
+for solution in solutions:
+    print(solution.magicConcat)
+    solutionConcats.append(solution.magicConcat)
+uniqueSolutions = set(solutionConcats)
+assert len(uniqueSolutions) == len(website_solutions)
+
+
+# Test real solution
+paths = [[0, 1, 2], [3, 2, 4], [5, 4, 6], [7, 6, 8], [9, 8, 1]]
+from itertools import permutations
+maxVal = 10
+valuesToTest = permutations(list(range(1,maxVal+1)))
+nodeList = list(range(maxVal+1))
+solutions = []
+for i, val in enumerate(valuesToTest):
+    pathVals = dict(zip(nodeList, val))
+    ring = createRing(pathVals, paths)
+    if ring.isMagic:
+        solutions.append(ring)
+
+
+solutionConcats = []
+for solution in solutions:
+    print(solution.magicConcat)
+    solutionConcats.append(solution.magicConcat)
+uniqueSolutions = set(solutionConcats)
+len(uniqueSolutions)
+
+# need only 16 digit solutions
+sixteen_sol = []
+for sol in uniqueSolutions:
+    if len(sol) == 16:
+        sixteen_sol.append(int(sol))
+
+sorted(sixteen_sol)[::-1]
+
 
 
